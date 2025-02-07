1ac9fa472a3ea2c7467286068138e184dbcd48b5
WELLINGTON RODRIGO CRUZ <wellington.rcruz@senacsp.edu.br>
2025-02-06 22:18:40 -0300

    app

---
+++ app.js
@@ -0,0 +1,24 @@
+/**
+ * APP PWA - Etanol x Gasolina
+ * @author Wellington R. Cruz
+ * @version 2.0
+ */
+
+function calcular () {
+    // capturar os valores das caixas de input
+    let gasolina = document.getElementById('gasolina').value
+    let etanol = document.getElementById('etanol').value
+    let kmlGasolina = document.getElementById('kmGasolina').value
+    let kmlEtanol = document.getElementById('kmEtanol').value
+    //console.log(gasolina)
+    // console.log(etanol)
+    // console.log(kmlGasolina)
+    // console.log(kmlEtanol)
+
+    // calculo da vantagem
+    if (etanol < (kmlEtanol / kmlGasolina) * gasolina) {
+        document.getElementById('status').src = "img/etanol.png"
+    } else {
+        document.getElementById('status').src = "img/gasolina.png"
+    }
+}
