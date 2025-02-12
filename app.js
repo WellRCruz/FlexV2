6e4343d503bdcc531565ceeefd9831b7b49e4d20
WELLINGTON RODRIGO CRUZ <wellington.rcruz@senacsp.edu.br>
2025-02-11 22:10:11 -0300

    App Service Worker

--- app.js
+++ app.js
@@ -4,7 +4,21 @@
  * @version 2.0
  */
 
-function calcular () {
+//================================================================
+// Registro do service worker
+
+// se o navegador de internet suportar este recurso
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/sw.js')
+        .then(() => {
+            console.log("Service Worker registrado!")
+        })
+
+}
+//=================================================================
+
+function calcular() {
     // capturar os valores das caixas de input
     let gasolina = document.getElementById('gasolina').value
     let etanol = document.getElementById('etanol').value
